15f1b500aeb9408ccd218619e58136200bc615d2
afonsocrg <afonso.crg@gmail.com>
2024-02-26 11:20:50 +0000

    remove unused import

--- frontend/src/pages/Todos/index.tsx
+++ frontend/src/pages/Todos/index.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { Button, Typography, List } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
