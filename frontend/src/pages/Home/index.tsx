9ce6bdec2d57b07cfaef425bcb75aab42899c346
afonsocrg <afonso.crg@gmail.com>
2024-02-26 00:14:19 +0000

    home page now renders project README

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,100 +1,45 @@
 import "./styles.css";
-import { Typography } from "antd";
+import { useState, useEffect } from "react";
+import { Typography, List } from "antd";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const { Title, Text } = Typography;
 
+const renderers = {
+  heading: function Heading(props) {
+    return <Title level={props.level}>{props.children}</Title>;
+  },
+  list: function MakeList(props) {
+    return <List bordered>{props.children}</List>;
+  },
+  listItem: function MakeListItem(props) {
+    return <List.Item>{props.children}</List.Item>;
+  },
+  inlineCode: function makeInlineCode(props) {
+    return <Text code>{props.children}</Text>;
+  },
+  // code: function makeCodeBlock(props) {
+  //     return <SyntaxHighlighter language={props.language} style={coy}>{props.value}</SyntaxHighlighter>
+  // },
+  blockquote: function makeBlockQuote(props) {
+    return <Text type="secondary">{props.children}</Text>;
+  },
+};
+
 export default function Home() {
+  const [markdown, setMarkdown] = useState(null);
+  useEffect(() => {
+    import("../../../../README.md").then((res) => {
+      fetch(res.default)
+        .then((response) => response.text())
+        .then((text) => setMarkdown(text))
+        .catch((error) => console.error(error));
+    });
+  }, []);
   return (
-    <>
-      <Title>Toy To Do App: Authentication Playground</Title>
-      <Text>
-        This Toy To Do app serves as a proof of concept for exploring various
-        aspects of user authentication and account management. The primary goal
-        of this project is to provide a playground environment where developers
-        can experiment with different authentication flows, token management
-        strategies, and user account functionalities.
-      </Text>
-      <Title level={2}>Features</Title>
-      <ul>
-        <li>
-          <Text>
-            <strong>User Registration</strong>: Users can sign up for a new
-            account by providing necessary information such as username, email,
-            and password.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>User Authentication</strong>: Registered users can log in
-            securely using their credentials.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>Token Management</strong>: Implementation of access token
-            expiration and refresh mechanism to ensure secure authentication.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>To Do Management</strong>: Users can create, delete, and
-            edit their to-do todos.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>Account Deletion</strong>: Capability for users to delete
-            their accounts and associated data.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>Password Recovery</strong>: Option for users to recover
-            their passwords in case they forget them.
-          </Text>
-        </li>
-      </ul>
-
-      <Title level={2}>Purpose</Title>
-      <Text>The purpose of this Toy To Do app is multifold:</Text>
-
-      <ul>
-        <li>
-          <Text>
-            <strong>Proof of Concept</strong>: It serves as a proof of concept
-            for implementing authentication mechanisms within an application.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>Learning Playground</strong>: Developers can utilize this
-            project to understand and experiment with various authentication
-            flows, including login/logout, token expiration, refresh tokens,
-            etc.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>Security Exploration</strong>: Provides a platform to
-            explore security best practices related to authentication and user
-            account management.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>API Integration Practice</strong>: Developers can practice
-            integrating authentication features into both frontend and backend
-            applications.
-          </Text>
-        </li>
-        <li>
-          <Text>
-            <strong>Conceptual Understanding</strong>: Helps in gaining a deeper
-            understanding of the concepts involved in user authentication and
-            authorization.
-          </Text>
-        </li>
-      </ul>
-    </>
+    <Markdown remarkPlugins={[remarkGfm]} components={{}}>
+      {markdown}
+    </Markdown>
   );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfig from './tsconfig.json'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfig from "./tsconfig.json";
 
 // Convert paths from tsconfig.json
 function getAlias() {
-  const paths = tsconfig.compilerOptions.paths
-  const alias = {}
+  const paths = tsconfig.compilerOptions.paths;
+  const alias = {};
   for (const path in paths) {
-    alias[path.replace('/*', '')] = paths[path][0].replace('/*', '').replace('.', '')
+    alias[path.replace("/*", "")] = paths[path][0]
+      .replace("/*", "")
+      .replace(".", "");
   }
-  return alias
+  return alias;
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.md"],
   resolve: {
     alias: getAlias(),
   },
-})
+});
